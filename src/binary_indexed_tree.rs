4e2e716b9cecb61d3f4842e5366b6a4fa9f9629b
YoshizawaShogo <shogo.yoshizawa.2000@gmail.com>
2024-02-15 18:19:39 +0900

    binary_indexed_treeを修正

--- src/binary_indexed_tree.rs
+++ src/binary_indexed_tree.rs
@@ -2,6 +2,8 @@ use cargo_snippet::snippet;
 
 
 #[snippet("binary_indexed_tree")]
+/// 遅延セグメント木よりも機能が少ないが、
+/// 使いやすく、高速である事も多い
 struct BinaryIndexedTree<A>(Vec<A>)
 where
     A: Sized + std::ops::AddAssign + Clone + Default;
@@ -18,8 +20,9 @@ where
 #[snippet("binary_indexed_tree")]
 impl<A> BinaryIndexedTree<A>
 where
-    A: Sized + std::ops::AddAssign + Clone + Default,
+    A: Sized + std::ops::AddAssign + Clone + Default + std::ops::Sub<Output = A>,
 {
+    /// i番目の要素にaddendを加算する
     fn add(&mut self, mut i: usize, addend: A) {
         debug_assert!(i < self.0.len());
         i += 1;
@@ -28,15 +31,22 @@ where
             i += lsb(i);
         }
     }
-    fn sum(&self, mut i: usize) -> A {
-        debug_assert!(i < self.0.len());
+    /// [0, r) の合計を求める
+    fn sum_0_to_r(&self, r: usize) -> A {
+        debug_assert!(r <= self.0.len());
+        let mut i = r;
         let mut sum = A::default();
-        i += 1;
         while i != 0 {
             sum += self.0[i - 1].clone();
             i -= lsb(i);
         }
         sum
+    }
+    /// [l, r) の合計値を取得
+    fn sum_l_to_r(&self, l: usize, r: usize) -> A {
+        debug_assert!(l <= r);
+        debug_assert!(r <= self.0.len());
+        self.sum_0_to_r(r) - self.sum_0_to_r(l)
     }
 }
 
